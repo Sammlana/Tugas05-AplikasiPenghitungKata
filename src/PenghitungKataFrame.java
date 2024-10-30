e980f213d887ba796e722dace7f325f05d63a7de
Sammlana <jokolkkpk@gmail.com>
2024-10-30 11:10:00 +0800

    menambahkan logika program dan event

--- src/PenghitungKataFrame.java
+++ src/PenghitungKataFrame.java
@@ -1,3 +1,7 @@
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,7 +19,25 @@ public class PenghitungKataFrame extends javax.swing.JFrame {
      */
     public PenghitungKataFrame() {
         initComponents();
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateCounts();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateCounts();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateCounts();
+            }
+        });
     }
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -29,13 +51,13 @@ public class PenghitungKataFrame extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        wordCountLabel = new javax.swing.JLabel();
         countButton = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        sentenceCountLabel = new javax.swing.JLabel();
+        charCountLabel = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        textArea = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,15 +67,25 @@ public class PenghitungKataFrame extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
         jLabel2.setText("Input Text");
 
-        jLabel3.setText("Jumlah Kata");
+        wordCountLabel.setText("Jumlah Kata: 0");
 
         countButton.setText("Hitung");
+        countButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                countButtonActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Keluar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
-        jLabel4.setText("Jumlah Kalimat");
+        sentenceCountLabel.setText("Jumlah Kalimat: 0");
 
-        jLabel5.setText("Jumlah Karakter");
+        charCountLabel.setText("Jumlah Karakter: 0");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -75,9 +107,9 @@ public class PenghitungKataFrame extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(47, 47, 47)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel4))))
+                            .addComponent(charCountLabel)
+                            .addComponent(wordCountLabel)
+                            .addComponent(sentenceCountLabel))))
                 .addGap(327, 327, 327))
         );
         jPanel1Layout.setVerticalGroup(
@@ -86,11 +118,11 @@ public class PenghitungKataFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jLabel1)
                 .addGap(57, 57, 57)
-                .addComponent(jLabel3)
+                .addComponent(wordCountLabel)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel5)
+                .addComponent(charCountLabel)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel4)
+                .addComponent(sentenceCountLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 142, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING)
@@ -99,9 +131,16 @@ public class PenghitungKataFrame extends javax.swing.JFrame {
                         .addComponent(countButton))))
         );
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        textArea.setColumns(20);
+        textArea.setRows(5);
+        textArea.addInputMethodListener(new java.awt.event.InputMethodListener() {
+            public void caretPositionChanged(java.awt.event.InputMethodEvent evt) {
+            }
+            public void inputMethodTextChanged(java.awt.event.InputMethodEvent evt) {
+                textAreaInputMethodTextChanged(evt);
+            }
+        });
+        jScrollPane1.setViewportView(textArea);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -121,6 +160,46 @@ public class PenghitungKataFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void updateCounts() {
+        String text = textArea.getText().trim();  // Menghapus spasi di awal dan akhir teks
+
+        // Cek apakah teks kosong
+        if (text.isEmpty()) {
+            wordCountLabel.setText("Jumlah Kata: 0");
+            charCountLabel.setText("Jumlah Karakter: 0");
+            sentenceCountLabel.setText("Jumlah Kalimat: 0");
+            return; // Keluar dari metode jika teks kosong
+        }
+
+        // Hitung jumlah kata
+        String[] words = text.split("\\s+");
+        int wordCount = words.length;
+
+        // Hitung jumlah karakter tanpa spasi
+        int charCount = text.length();
+
+        // Hitung jumlah kalimat
+        String[] sentences = text.split("[.!?]");
+        int sentenceCount = sentences.length;
+
+        // Update label dengan hasil perhitungan
+        wordCountLabel.setText("Jumlah Kata: " + wordCount);
+        charCountLabel.setText("Jumlah Karakter: " + charCount);
+        sentenceCountLabel.setText("Jumlah Kalimat: " + sentenceCount);
+        }
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void countButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_countButtonActionPerformed
+        updateCounts();
+    }//GEN-LAST:event_countButtonActionPerformed
+
+    private void textAreaInputMethodTextChanged(java.awt.event.InputMethodEvent evt) {//GEN-FIRST:event_textAreaInputMethodTextChanged
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textAreaInputMethodTextChanged
 
     /**
      * @param args the command line arguments
@@ -158,15 +237,15 @@ public class PenghitungKataFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel charCountLabel;
     private javax.swing.JButton countButton;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JLabel sentenceCountLabel;
+    private javax.swing.JTextArea textArea;
+    private javax.swing.JLabel wordCountLabel;
     // End of variables declaration//GEN-END:variables
 }
